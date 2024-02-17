a6d5d669f30426edd481d3c49cdd7f0d044a61bd
Yurii <152527309+Waitin4Silksong@users.noreply.github.com>
2024-02-17 23:43:04 +0300

    Add files via upload

---
+++ dz.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+#include <string>
+
+class Animal
+{
+protected:
+    std::string name;
+    std::string type;
+    double age;
+    std::string breed;
+public:
+    Animal(const std::string& name, const std::string& type, double age, const std::string& desc) : 
+        name{ name }, type{ type }, age{ age }, breed{ breed } {}
+    virtual void print()
+    {
+        std::cout << name << std::endl;
+        std::cout << type << std::endl;
+        std::cout << age << " y/o" << std::endl;
+        std::cout << breed;
+    }
+};
+class Cat : public Animal
+{
+private:
+    bool vacc;
+public:
+    Cat(const std::string& name, double age, const std::string& breed, bool vacc) :
+        Animal{ name, "Cat", age, breed }, vacc{vacc} {}
+    void print() override
+    {
+        Animal::print();
+        if (vacc == true)
+        {
+            std::cout << type << " is vaccinated" << std::endl;
+        }
+        else
+        {
+            std::cout << type << " is not vaccinated" << std::endl;
+        }
+    }
+};
+class Dog : public Animal
+{
+private:
+    bool vacc;
+public:
+    Dog(const std::string& name, double age, const std::string& breed, bool vacc) :
+        Animal{ name, "Dog", age, breed }, vacc{ vacc } {}
+    void print() override
+    {
+        Animal::print();
+        if (vacc == true)
+        {
+            std::cout << type << " is vaccinated" << std::endl;
+        }
+        else
+        {
+            std::cout << type << " is not vaccinated" << std::endl;
+        }
+    }
+};
+class Parrot : public Animal
+{
+private:
+    bool vacc;
+public:
+    Parrot(const std::string& name, double age, const std::string& breed, bool vacc) :
+        Animal{ name, "Parrot", age, breed }, vacc{vacc} {}
+    void print() override
+    {
+        Animal::print();
+        if (vacc == true)
+        {
+            std::cout << type << " is vaccinated" << std::endl;
+        }
+        else
+        {
+            std::cout << type << " is not vaccinated" << std::endl;
+        }
+    }
+};
+
+
+int main()
+{
+    Cat cat("Tyson", 3.4, "Siamese", true);
+    Dog dog("Mira", 7.0, "Pug", true);
+    Parrot parrot("Sunny", 2.2, "Budgerigar", false);
+    cat.print();
+    std::cout << std::endl;
+    dog.print();
+    std::cout << std::endl;
+    parrot.print();
+}
---
+++ tasks(class_work).cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <string>
+
+class Text
+{
+protected:
+	std::string text;
+public:
+	// Text() : text("") {};
+	Text(const std::string& text) : text{ text } {}
+	virtual void print()
+	{
+		std::cout << text << std::endl;
+	}
+};
+class GreatTask : public Text
+{
+public:
+	GreatTask(const std::string& text) : Text{ text } {}
+	virtual void print() override
+	{
+		std::cout << "[!] " << text << " [!]" << std::endl;
+	}
+};
+class DeadlineTask : public Text
+{
+private:
+	int day;
+	int month;
+public:
+	DeadlineTask(const std::string& text, int day, int month) : Text{ text }, day{ day }, month{ month } {}
+	virtual void print() override
+	{
+		std::cout << text << " (Deadline: " << day << "." << month << ")" << std::endl;
+	}
+};
+class DailyTask : public Text
+{
+private:
+	int hours;
+	int minutes;
+public:
+	DailyTask(const std::string& text, int hours, int minutes) : Text{ text }, hours{ hours }, minutes{ minutes } {}
+	DailyTask(const std::string& text, int hours) : Text{ text }, hours{ hours }, minutes{ 00 } {}
+	virtual void print() override
+	{
+		std::cout << text << " (Time: " << hours << ":" << minutes << ")" << std::endl;
+	}
+};
+
+
+
+int main()
+{
+	Text* all[5];
+	all[0] = new Text("Go to school");
+	all[1] = new GreatTask("Write an essay");
+	all[2] = new DeadlineTask("Homework", 16, 02);
+	all[3] = new DailyTask("Laundry", 13, 30);
+	all[4] = new DailyTask("Shower", 16);
+	for (size_t i = 0; i < 5; i++)
+	{
+		all[i]->print();
+	}
+}
